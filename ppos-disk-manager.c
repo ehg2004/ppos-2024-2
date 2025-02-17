588b55cac39f21ee7bf95593f8624af7dc903304
thaSay <thaiscedric3@gmail.com>
2025-02-16 22:16:03 -0300

    added CSCAN and SSTF

--- ppos-disk-manager.c
+++ ppos-disk-manager.c
@@ -7,20 +7,41 @@
 #include "ppos-disk-manager.h"
 
 // adicione todas as variaveis globais necessarias para implementar o gerenciado do disco
+#define FCFS 0
+#define CSCAN 1
+#define SSTF 2
+int scheduler_algorithm = FCFS;
 disk_t disk;
+int position;
+int total_distance;
+task_t* disk_mgr_task;
 
 
-void bodyDiskManager(void* arg);
+diskrequest_t* disk_schedulerSSTF(diskrequest_t* queue);
+diskrequest_t* disk_schedulerCSCAN(diskrequest_t* queue);
+diskrequest_t* disk_schedulerFCFS(diskrequest_t* queue);
+
+void bodyDiskManager(void* arg)  {
+
+}
 void diskSignalHandler() {
     sem_down(&(disk.disk_semaphore));
     task_t* ready_task = (task_t*)queue_remove((queue_t**)&(disk.disk_task_queue), (queue_t*)disk.disk_task);
     if(ready_task != NULL) task_resume(ready_task);
-    diskrequest_t* next_request = disk_scheduler(disk.disk_queue);
+    diskrequest_t* next_request;
+    if(scheduler_algorithm == FCFS)
+        next_request = disk_schedulerFCFS(disk.disk_queue);
+    else if(scheduler_algorithm == CSCAN)
+        next_request = disk_schedulerCSCAN(disk.disk_queue);
+    else if(scheduler_algorithm == SSTF)
+        next_request = disk_schedulerSSTF(disk.disk_queue);
     if(next_request != NULL) {
         if(next_request->request_type == 1) {
             disk_cmd(DISK_CMD_READ, next_request->request_block, next_request->request_buffer);
         }
         else  disk_cmd(DISK_CMD_WRITE, next_request->request_block, next_request->request_buffer);
+        total_distance += (next_request->request_block >= position)?(next_request->request_block - position):(position - next_request->request_block );
+        position = next_request->request_block;
         disk.disk_task = next_request->request_task;
         task_suspend(taskExec, &(disk.disk_task_queue));
     }
@@ -37,16 +58,22 @@ void clean_exit_on_sig(int sig_num) {
 int disk_mgr_init (int *numBlocks, int *blockSize) {
     // coloque o codigo para inicializar o disco aqui
     if(numBlocks == NULL || blockSize == NULL) return -1;
+    
     if(disk_cmd (DISK_CMD_INIT, 0, 0) < 0) return -1;
+    
     if(*(numBlocks) = disk_cmd (DISK_CMD_DISKSIZE, 0, 0), *(numBlocks) < 0) return -1;
     if(*(blockSize) = disk_cmd (DISK_CMD_BLOCKSIZE, 0, 0), *(blockSize) < 0) return -1;
     disk.n_blocks = (*numBlocks);
     disk.blocks_sz = (*blockSize);
+    
     if(sem_create(&(disk.disk_semaphore), 1) < 0) return -1;
     if(sem_create(&(disk.disk_queue_semaphore), 1) < 0) return -1;
+    
     disk.disk_queue = NULL;
     disk.disk_task = NULL;
     disk.disk_task_queue = NULL;
+    
+    position = 0;
 
     signal(SIGUSR1, diskSignalHandler);
     // o seu codigo deve terminar ate aqui. 
@@ -108,7 +135,7 @@ int disk_block_write(int block, void* buffer) {
 // Essa função implemeneta o escalonador de requisicoes de 
 // leitura/scrita do disco usado pelo gerenciador do disco
 // A função implementa a política FCFS.
-diskrequest_t* disk_scheduler(diskrequest_t* queue) {
+diskrequest_t* disk_schedulerFCFS(diskrequest_t* queue) {
      // FCFS scheduler
     if ( queue != NULL ) {
         PPOS_PREEMPT_DISABLE
@@ -117,4 +144,43 @@ diskrequest_t* disk_scheduler(diskrequest_t* queue) {
         return request;
     }
     return NULL;
+}
+diskrequest_t* disk_schedulerCSCAN(diskrequest_t* queue) {
+    // FCFS scheduler
+   if ( queue != NULL ) {
+       PPOS_PREEMPT_DISABLE
+       diskrequest_t* request = queue;
+       int best_block = request->request_block;
+       queue = queue->next;
+       while(queue != NULL) {
+            if(queue->request_block < best_block) {
+                best_block = queue->request_block;
+                request = queue;
+            }
+            queue = queue->next;
+       }
+       PPOS_PREEMPT_ENABLE
+       return request;
+   }
+   return NULL;
+}
+diskrequest_t* disk_schedulerSSTF(diskrequest_t* queue) {
+    // FCFS scheduler
+   if ( queue != NULL ) {
+       PPOS_PREEMPT_DISABLE
+       diskrequest_t* request = queue;
+       int best_dist = (position >= request->request_block) ? (position - request->request_block):(request->request_block - position);
+       queue = queue->next;
+       while(queue != NULL) {
+            int dist = (position >= queue->request_block) ? (position - queue->request_block):(queue->request_block - position);
+            if(dist < best_dist) {
+                best_dist = dist;
+                request = queue;
+            }
+            queue = queue->next;
+       }
+       PPOS_PREEMPT_ENABLE
+       return request;
+   }
+   return NULL;
 }
